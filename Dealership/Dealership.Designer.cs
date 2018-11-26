647b5cb3ec606334b841c32ef5129fa0d0125e21
Andrew <amclaug7@dtcc.edu>
2018-11-26 10:43:11 -0500

    Add project files.

---
+++ Dealership/Dealership.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Dealership
+{
+    partial class Dealership
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lbxCustomers = new System.Windows.Forms.ListBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lbxInventory = new System.Windows.Forms.ListBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.lbxSales = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(552, 280);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(544, 254);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lbxCustomers);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(544, 254);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Customer";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lbxCustomers
+            // 
+            this.lbxCustomers.FormattingEnabled = true;
+            this.lbxCustomers.Location = new System.Drawing.Point(7, 7);
+            this.lbxCustomers.Name = "lbxCustomers";
+            this.lbxCustomers.Size = new System.Drawing.Size(517, 225);
+            this.lbxCustomers.TabIndex = 0;
+            this.lbxCustomers.DoubleClick += new System.EventHandler(this.lbxCustomers_DoubleClick);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.lbxInventory);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(544, 254);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Inventory";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lbxInventory
+            // 
+            this.lbxInventory.FormattingEnabled = true;
+            this.lbxInventory.Location = new System.Drawing.Point(7, 7);
+            this.lbxInventory.Name = "lbxInventory";
+            this.lbxInventory.Size = new System.Drawing.Size(480, 199);
+            this.lbxInventory.TabIndex = 0;
+            this.lbxInventory.DoubleClick += new System.EventHandler(this.lbxInventory_DoubleClick);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button1);
+            this.tabPage4.Controls.Add(this.lbxSales);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(544, 254);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Sales";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // lbxSales
+            // 
+            this.lbxSales.FormattingEnabled = true;
+            this.lbxSales.Location = new System.Drawing.Point(7, 7);
+            this.lbxSales.Name = "lbxSales";
+            this.lbxSales.Size = new System.Drawing.Size(120, 95);
+            this.lbxSales.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Dealership
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 302);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Dealership";
+            this.Text = "Toyota Dealer";
+            this.Load += new System.EventHandler(this.Dealership_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox lbxCustomers;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ListBox lbxInventory;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbxSales;
+    }
+}
+
