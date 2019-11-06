ebeb2a7c3d298ee869f4f0601b83ec29ccde2829
Andrew <amclaug7@dtcc.edu>
2019-11-06 01:35:44 -0500

    temp

--- Dealership/LogInPage.cs
+++ Dealership/LogInPage.cs
@@ -2,6 +2,8 @@
 using System.Data;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Dealership
 {
@@ -23,12 +25,24 @@ namespace Dealership
         private void btnCheckCredentials_Click(object sender, EventArgs e)
         {
             var f = new SalesRecords();
-            var g = new FactoryOrder();
+            var g = new FactoryOrder();			
 
-            string username = tbxUsername.Text;
+			string username = tbxUsername.Text;
             string password = tbxPassword.Text;
 
-            string query = $"select username, password, employeetype from employee " +
+			string sha256(string randomString)
+			{
+				var crypt = new SHA256Managed();
+				var hash = new StringBuilder();
+				byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+				foreach (byte theByte in crypto)
+				{
+					hash.Append(theByte.ToString("x2"));
+				}
+				return hash.ToString();
+			}
+
+			string query = $"select username, password, employeetype from employee " +
                            $"where username = '{username}'";
             SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
             DataTable dtbl = new DataTable();
@@ -38,7 +52,7 @@ namespace Dealership
             {
                 foreach (DataRow row in dtbl.Rows)
                 {
-                    if (row["password"].ToString().Trim() != password)
+                    if (row["password"].ToString().Trim() != sha256(password))
                     {
                         MessageBox.Show("Incorrect Password", "Error", MessageBoxButtons.OK,
                             MessageBoxIcon.Error);
